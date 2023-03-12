0812b9ed9a2fa898cc0951eb8b7de294c3a48927
suah lee <suah216@gmail.com>
2023-03-12 22:16:17 +0900

    first

---
+++ main.js
@@ -0,0 +1,84 @@
+// 필요한 html elements 다 가져오기
+let computerNumber = 0;
+let playButton = document.getElementById("play-button");
+let resetButton = document.querySelector(".button-reset");
+let userInput = document.querySelector("#user-input");
+let resultAreaImg = document.querySelector(".main-img");
+let resultText = document.querySelector(".result-text");
+let chanceArea = document.getElementById("chance-area");
+let gameOver = false;
+let chances = 5; // 남은 기회
+let userValueList = []; // 유저가 입력한 숫자들 리스트
+
+chanceArea.innerHTML = `남은 기회:${chances}`;
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", focusInput);
+
+function pickRandomNumber() {
+  // 랜덤숫자 뽑기
+
+  computerNumber = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", computerNumber);
+}
+
+function play() {
+  // 숫자 추측하기
+  const userValue = userInput.value;
+  if (userValue < 1 || userValue > 100) {
+    resultText.textContent = "1부터 100 사이의 숫자를 입력 해주세요";
+
+    return;
+  }
+
+  if (userValueList.includes(userValue)) {
+    resultText.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
+
+    return;
+  }
+
+  chances--;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList.push(userValue);
+  if (userValue < computerNumber) {
+    resultAreaImg.src =
+      "https://media0.giphy.com/media/iZGpuaRKdEZoI/giphy.gif";
+    resultText.textContent = "Up!";
+  } else if (userValue > computerNumber) {
+    resultAreaImg.src = "https://media1.giphy.com/media/5QQ6FpAQ0syYLkONPB/giphy.gif";
+    resultText.textContent = "Down!";
+  } else {
+    resultAreaImg.src =
+      "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif";
+    resultText.textContent = "정답!";
+    gameOver = true;
+  }
+
+  if (chances == 0) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function focusInput() {
+  userInput.value = "";
+}
+
+function reset() {
+  //리셋
+  pickRandomNumber();
+  userInput.value = "";
+  resultAreaImg.src =
+    "https://profileimg.plaync.com/game_profile_images/aion/images?gameServerKey=25&charKey=290595";
+  resultText.textContent = "다시시작 숫자게임";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList = [];
+}
+
+pickRandomNumber();
